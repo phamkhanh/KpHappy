aed21d2a6c6edc85b7474980f03f976bc61cb297
Khanh Pham <phamkhanh1811@gamil.com>
2018-01-06 22:06:10 +0700

    install package Microsoft.Owin.Host.SystemWeb; test api controller successfull

---
+++ KpHappy.Data/Repositories/PostCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using KpHappy.Data.Infrastructure;
+using KpHappy.Model.Models;
+
+namespace KpHappy.Data.Repositories
+{
+    public interface IPostCategoryRepository : IRepository<PostCategory>
+    {
+    }
+
+    public class PostCategoryRepository : RepositoryBase<PostCategory>, IPostCategoryRepository
+    {
+        public PostCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
