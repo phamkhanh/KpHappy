7ebe77a4068b7aa4d8223eff8f3ed8486bb29e6b
Khanh Pham <phamkhanh1811@gamil.com>
2018-01-06 21:54:13 +0700

    install Autofac; Autofac.Mvc5; Autofac.WebApi2 add startup.cs file

---
+++ KpHappy.Web/App_Start/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+using System.Reflection;
+using KpHappy.Data.Infrastructure;
+using KpHappy.Data.Repositories;
+using KpHappy.Service;
+using System.Web.Mvc;
+using System.Web.Http;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using KpHappy.Data;
+
+[assembly: OwinStartup(typeof(KpHappy.Web.App_Start.Startup))]
+
+namespace KpHappy.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //Register WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<KpHappyDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(ErrorRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(ErrorService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
+
+        }
+    }
+}
